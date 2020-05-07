7bea7733b2a977129f233ce8d0420f8b9e19b65d
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-05-07 18:20:08 -0400

    new script

---
+++ scriptobjcl.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hi(self):
+        print('Hello, my name is', self.name)
+p
+    def howold(self):
+        print('I am', self.age)
+
+class Teacher:
+    def __init__(self, name, teachfper, subject)
